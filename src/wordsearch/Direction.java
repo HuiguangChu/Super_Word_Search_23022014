b13ba013bc52bf8af42f4e1b1f4c1622c338158b
Huiguang Chu <huiguangchu@gmail.com>
2014-02-23 13:36:00 +0100

    source code
    define 8 searching directions

---
+++ src/wordsearch/Direction.java
@@ -0,0 +1,14 @@
+/*define the eight directions in  enum. */
+
+package wordsearch;
+
+public  enum Direction {
+	RL, //right to left
+    BT,//bottom to top 
+    LR, //left to right
+    TB, //top to bottom
+    BLTR, //bottom left to top right
+    BRTL,// bottom right to top left
+    TLBR, //top left to bottom right
+    TRBL //top right to bottom left
+}
